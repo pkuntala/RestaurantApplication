d8124a49a09dcd50faf068437f1d1ae02abb1ae3
tejasvi <tejasvi.sri.kanaka.lakshmi.murukuri@pwc.com>
2021-10-14 11:59:33 +0530

    Initial Commit

---
+++ RestaurantApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by Murukuri Tejasvi Sri Kanaka Lakshmi  on 14/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
